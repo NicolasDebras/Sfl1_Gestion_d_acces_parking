906b2d0533f20682f7cc8262ae26307be9f95e1b
coco196 <65906610+coco196@users.noreply.github.com>
2021-04-02 10:40:34 +0200

    modif html+css

--- site/main.php
+++ site/main.php
@@ -2,10 +2,10 @@
 ?>
 <!DOCTYPE html>
 <?php
-$PARAM_hote='localhost'; // le chemin vers le serveur
+$PARAM_hote='10.6.0.1'; // le chemin vers le serveur
 $PARAM_port='3306';
 $PARAM_nom_bd='parking'; // le nom de votre base de données
-$PARAM_utilisateur='administrateur_parking'; // nom d'utilisateur pour se connecter
+$PARAM_utilisateur='parking'; // nom d'utilisateur pour se connecter
 $PARAM_mot_passe='Nantes44'; // mot de passe de l'utilisateur pour se connecter
 $connexion = new PDO('mysql:host='.$PARAM_hote.';port='.$PARAM_port.';dbname='.$PARAM_nom_bd, $PARAM_utilisateur, $PARAM_mot_passe);
 ?>
@@ -17,23 +17,20 @@ $connexion = new PDO('mysql:host='.$PARAM_hote.';port='.$PARAM_port.';dbname='.$
     <header class="page-header"> 
         
             <meta charset="utf-8" />
-           
-            
-            <h1> OCCUPATION PARKING ST FELIX </h1> <img src="image\logo_st_felix.png" width="190px"  />
-            
-        
-        
-        
-              
+                     
+            <h1> OCCUPATION PARKING ST-FELIX </h1> <img src="image\logo_st_felix.png" width="190px"  />
+                        
     </header>
     <body>
             <main>
                
             
             <?php
-            echo date('d/m/Y h:i:s'); 
             $resultats = $connexion->query("SELECT * FROM place ");
             $resultats->setFetchMode(PDO::FETCH_OBJ);
+            date_default_timezone_set("Europe/Paris");
+            $date = date("d/m/Y");
+            $heure = date("H:i");
 
            $a = 0;
             
@@ -44,30 +41,43 @@ $connexion = new PDO('mysql:host='.$PARAM_hote.';port='.$PARAM_port.';dbname='.$
                 $etat = $place->etat;
                 
                 
-                if($etat == 1)
+                
+                if($etat === "occupe")
                 {
                     $a = $a + 1;
                 }
                 
-                
-
             }
             $diff = 6 - $a;
             
             if($diff > 0)
             {
-                 echo '<p class="texte-centre">place dispo : ' . $diff . '</p>';
-                 ?><img src="image\rond_vert.png"/><?php
+                ?>
+                <div style="left: 50%; transform: translate(-50%); position: absolute; bottom: 37%;  z-index: 2;">
+                <?php echo '<p> <font size="10" face="verdana" color="white"> Place disponible : ' . $diff . '</br> Nous sommes le : '.$date. '</br> il est : ' .$heure. '</font> </p>' ?>
+                </div>
+                
+                <div class="test">
+                <img src="image\rond_vert.png"/>
+                </div>
+                    <?php
             }
             else
             {
-                echo '<p> parking pleins </p>';
-                echo '<p class="texte-centre">place dispo : ' . $diff . '</p>';
-                ?><img src="image\rond_rouge.png"/><?php
+                ?>
+                <div style="left: 50%; transform: translate(-50%); position: absolute; bottom: 37%;  z-index: 2;">
+                <?php echo '<p> <font size="10" face="verdana" color="white"> Parking pleins </br> Nous sommes le : ' .$date. ' </br> il est : ' .$heure. '</font> </p>';?>
+                </div>
+
+                <div class="test">
+                <img src="image\rond_rouge.png"/>
+                </div>    
+                    <?php
             }
             ?>
 
         </main>
+        
         
     </body>
         
