7a25e0f1e3a00a0402b8b4c449213c7a1c608b8d
Nicolas <nicodebras17@gmail.com>
2021-05-31 16:58:07 +0200

    upgrade unit test

--- Application C#/APP_C_PARKING/UnitTestProject1/UnitTest1.cs
+++ Application C#/APP_C_PARKING/UnitTestProject1/UnitTest1.cs
@@ -5,18 +5,20 @@ using APP_C_PARKING;
 namespace UnitTestProject1
 {
     [TestClass]
+
+    /*
+     * Méthode de test correspondant à La classe Connexion
+     */
     public class UnitTest1
     {
         string badge = "4851274";
         string chaine = "CAPS                     Corentin                 4851274";
 
-        string badge_delete = " ";
-                        
+        Connexion co = new Connexion();
 
         [TestMethod]
         public void TestMethod_verification()
         {
-            Connexion co = new Connexion();
             bool n = co.verification(badge);
             Assert.AreEqual(n, false);
         }
@@ -25,7 +27,6 @@ namespace UnitTestProject1
 
         public void TestMethod_recup_info_badge()
         {
-            Connexion co = new Connexion();
             string test = co.recup_info_badge();
             Assert.AreEqual(test, chaine);
         }
@@ -34,19 +35,15 @@ namespace UnitTestProject1
 
         public void TestMethod_Creation_user()
         {
-            Connexion co = new Connexion();
             co.add_user("debras", "nicolas", "555555", "user");
             bool n = co.verification("555555");
             Assert.AreEqual(n, false);
         }
 
-
-
         [TestMethod]
 
         public void TestMethod_delete_user()
         {
-            Connexion co = new Connexion();
             co.delete_user("555555");
             bool n = co.verification("555555");
             Assert.AreEqual(n, true);
